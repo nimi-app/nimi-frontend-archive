f6efecda7216749afe18fdee79f2e459492e839e
Violette <milanamsterdam@Milans-MacBook-Pro.local>
2022-04-23 01:18:57 +0200

    feautre: Added ens domains fetching hook

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -280,7 +280,7 @@ function App(props) {
       <Switch>
         <Route exact path="/">
           {/* pass in any web3 props to this Home component. For example, yourLocalBalance */}
-          <Home yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
+          <Home address={address} yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
         </Route>
         <Route exact path="/debug">
           {/*
--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -2,6 +2,7 @@ import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
 import React from "react";
 import { Link } from "react-router-dom";
+import useEnsDomains from "../hooks/useEnsDomains";
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -9,11 +10,13 @@ import { Link } from "react-router-dom";
  * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
  * @returns react component
  **/
-function Home({ yourLocalBalance, readContracts }) {
+function Home({ yourLocalBalance, readContracts, address }) {
   // you can also use hooks locally in your component of choice
   // in this case, let's keep track of 'purpose' variable from our contract
   const purpose = useContractReader(readContracts, "YourContract", "purpose");
-
+  console.log("address in home", address);
+  const ens = useEnsDomains(address);
+  console.log(ens);
   return (
     <div>
       <div style={{ margin: 32 }}>
