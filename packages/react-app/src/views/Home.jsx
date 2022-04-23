f33fee1d36cdc4034751116dbdf5411bef27dfd6
Violette <milanamsterdam@Milans-MacBook-Pro.local>
2022-04-23 21:10:43 +0200

    added cards for all ens domains

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,10 +1,95 @@
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import nameHash from "@ensdomains/eth-ens-namehash";
 import { ethers } from "ethers";
-import React from "react";
+import React, { useState } from "react";
 import contentHash from "content-hash";
 
 import useEnsDomains from "../hooks/useEnsDomains";
+import styled from "styled-components";
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 19px;
+  justify-content: center;
+`;
+const StyledDomainText = styled.div`
+  font-family: "Baloo 2";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 100%;
+  /* or 32px */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  /* gradient */
+
+  background: linear-gradient(111.35deg, #4368ea -25.85%, #c490dd 73.38%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+`;
+const StyledCard = styled.div`
+  display: flex;
+  /* height: 347px;
+
+  width: 337px; */
+  align-items: center;
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: 0px 5px 24px rgba(138, 143, 234, 0.12);
+  backdrop-filter: blur(20px);
+  /* Note: backdrop-filter has minimal browser support */
+  flex-direction: column;
+  border-radius: 25px;
+  border-radius: 25px;
+  padding: 64px;
+`;
+const SetupButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 24px;
+  margin-top: 42px;
+  margin-bottom: 26px;
+  position: static;
+  width: 209px;
+  height: 48px;
+
+  /* gradient */
+
+  background: linear-gradient(111.35deg, #4368ea -25.85%, #c490dd 73.38%);
+  border-radius: 30px;
+  font-family: "Baloo 2";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 28px;
+  /* identical to box height, or 156% */
+
+  /* white */
+
+  color: #ffffff;
+`;
+const GoToText = styled.div`
+  font-family: "Baloo 2";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 28px;
+  /* identical to box height, or 156% */
+
+  /* gradient */
+
+  background: linear-gradient(111.35deg, #4368ea -25.85%, #c490dd 73.38%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+`;
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -32,6 +117,11 @@ function Home({ address, userSigner }) {
 
   console.log("address in home", address);
   const { domains, loadingState } = useEnsDomains(address);
+  const [selectedDomain, setSelectedDomain] = useState(null);
+  const handleClick = ens => {
+    window.open(`https://${ens}.link`);
+  };
+  const [twitter, setTwitter] = useState("");
   console.log(domains, loadingState);
   console.log("yourLocalBalance", ethers);
   const asyncFunc = async () => {
@@ -46,8 +136,21 @@ function Home({ address, userSigner }) {
   };
   return (
     <div>
-      <h1>Home</h1>
-      <div>{!loadingState && domains[0].labelName}</div>
+      <FlexWrapper>
+        {!loadingState &&
+          domains.map(item => {
+            return (
+              <StyledCard onClick={() => setSelectedDomain(item.name)}>
+                <StyledDomainText>{item.name}</StyledDomainText>
+                <SetupButton>Set up a Nimi Profile</SetupButton>
+                <GoToText onClick={() => handleClick(item.name)}>Go to {item.name}</GoToText>
+              </StyledCard>
+            );
+          })}
+      </FlexWrapper>
+      <div>SelectedDomain:{selectedDomain}</div>
+      <Input.TextArea onChange={event => setTwitter(event)}>Twitter</Input.TextArea>
+      <div>Twiter name: {twitter}</div>
       <Button onClick={asyncFunc}>Deploy to ipfs</Button>
     </div>
   );
