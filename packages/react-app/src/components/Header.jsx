a12791af9f129147d98430b0e65cf5456455359a
Violette <milanamsterdam@Milans-MacBook-Pro.local>
2022-04-23 12:32:36 +0200

    added styled

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,23 +1,12 @@
-import { PageHeader } from "antd";
 import React from "react";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+`;
 
 // displays a page header
 
-export default function Header({link, title, subTitle}) {
-  return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
-      <PageHeader
-        title={title}
-        subTitle={subTitle}
-        style={{ cursor: "pointer" }}
-      />
-    </a>
-  );
-}
-
-
-Header.defaultProps = {
-  link: "https://github.com/austintgriffith/scaffold-eth",
-  title: "🏗 scaffold-eth",
-  subTitle: "forkable Ethereum dev stack focused on fast product iteration",
+export default function Header({ link, title, subTitle }) {
+  return <HeaderWrapper>Header wrapper</HeaderWrapper>;
 }
