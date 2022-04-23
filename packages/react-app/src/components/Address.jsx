5ca74701767110ed6347a79d1421872779bff5f9
Violette <milanamsterdam@Milans-MacBook-Pro.local>
2022-04-23 15:50:38 +0200

    feature: Navigation

--- packages/react-app/src/components/Address.jsx
+++ packages/react-app/src/components/Address.jsx
@@ -1,13 +1,12 @@
 import { Skeleton, Typography } from "antd";
 import React from "react";
-import { useThemeSwitcher } from "react-css-theme-switcher";
+import styled from "styled-components";
 import Blockies from "react-blockies";
 import { useLookupAddress } from "eth-hooks/dapps/ens";
 
 // changed value={address} to address={address}
 
-const { Text } = Typography;
-
+const StyledAddress = styled.div``;
 /** 
   ~ What it does? ~
 
@@ -34,7 +33,6 @@ const { Text } = Typography;
 const blockExplorerLink = (address, blockExplorer) => `${blockExplorer || "https://etherscan.io/"}address/${address}`;
 
 export default function Address(props) {
-  const { currentTheme } = useThemeSwitcher();
   const address = props.value || props.address;
   const ens = useLookupAddress(props.ensProvider, address);
   console.log("sdhshdhsdhsdhsdhhds", ens);
@@ -62,12 +60,7 @@ export default function Address(props) {
   if (props.minimized) {
     return (
       <span style={{ verticalAlign: "middle" }}>
-        <a
-          style={{ color: currentTheme === "light" ? "#222222" : "#ddd" }}
-          target="_blank"
-          href={etherscanLink}
-          rel="noopener noreferrer"
-        >
+        <a target="_blank" href={etherscanLink} rel="noopener noreferrer">
           <Blockies seed={address.toLowerCase()} size={8} scale={2} />
         </a>
       </span>
@@ -76,32 +69,19 @@ export default function Address(props) {
 
   return (
     <span>
-      <span style={{ verticalAlign: "middle" }}>
-        <Blockies seed={address.toLowerCase()} size={8} scale={props.fontSize ? props.fontSize / 7 : 4} />
-      </span>
       <span style={{ verticalAlign: "middle", paddingLeft: 5, fontSize: props.fontSize ? props.fontSize : 28 }}>
         {props.onChange ? (
-          <Text editable={{ onChange: props.onChange }} copyable={{ text: address }}>
-            <a
-              style={{ color: currentTheme === "light" ? "#222222" : "#ddd" }}
-              target="_blank"
-              href={etherscanLink}
-              rel="noopener noreferrer"
-            >
+          <StyledAddress editable={{ onChange: props.onChange }} copyable={{ text: address }}>
+            <a target="_blank" href={etherscanLink} rel="noopener noreferrer">
               {displayAddress}
             </a>
-          </Text>
+          </StyledAddress>
         ) : (
-          <Text copyable={{ text: address }}>
-            <a
-              style={{ color: currentTheme === "light" ? "#222222" : "#ddd" }}
-              target="_blank"
-              href={etherscanLink}
-              rel="noopener noreferrer"
-            >
+          <StyledAddress copyable={{ text: address }}>
+            <a target="_blank" href={etherscanLink} rel="noopener noreferrer">
               {displayAddress}
             </a>
-          </Text>
+          </StyledAddress>
         )}
       </span>
     </span>
--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,12 +1,62 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { ReactComponent as NimiLogo } from "../images/nimi-logo.svg";
+import Account from "./Account";
 
 const HeaderWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 27px;
+`;
+const LeftSide = styled.div`
+  display: flex;
+  gap: 25px;
+  align-items: center;
+`;
+const NavigationText = styled.div`
+  background: linear-gradient(154.32deg, #4368ea 0.48%, #c490dd 85.86%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
 `;
 
 // displays a page header
 
-export default function Header({ link, title, subTitle }) {
-  return <HeaderWrapper>Header wrapper</HeaderWrapper>;
+export default function Header({
+  useBurner,
+  address,
+  userSigner,
+  localProvider,
+  mainnetProvider,
+  price,
+  minimized,
+  web3Modal,
+  loadWeb3Modal,
+  logoutOfWeb3Modal,
+  blockExplorer,
+  isContract,
+}) {
+  return (
+    <HeaderWrapper>
+      <LeftSide>
+        <NimiLogo />
+        <NavigationText>Overview</NavigationText>
+        <NavigationText>Features</NavigationText>
+      </LeftSide>
+
+      <Account
+        address={address}
+        localProvider={localProvider}
+        userSigner={userSigner}
+        mainnetProvider={mainnetProvider}
+        price={price}
+        web3Modal={web3Modal}
+        loadWeb3Modal={loadWeb3Modal}
+        logoutOfWeb3Modal={logoutOfWeb3Modal}
+        blockExplorer={blockExplorer}
+      />
+    </HeaderWrapper>
+  );
 }
